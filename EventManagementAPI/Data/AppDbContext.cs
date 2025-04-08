e4bd3dc989c53c2769fd965919055dd44fad059a
kev <begot.kevin@gmail.com>
2025-04-08 17:17:43 +0200

    [ADD] dbcontexte

---
+++ EventManagementAPI/Data/AppDbContext.cs
@@ -0,0 +1,112 @@
+using EventManagementAPI.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventManagementAPI.Data
+{
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<SessionSpeaker> SessionSpeakers { get; set; }
+        public DbSet<Speaker> Speakers { get; set; }
+        public DbSet<EventParticipant> EventParticipants { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Relations Many-to-Many
+            modelBuilder.Entity<EventParticipant>()
+                .HasKey(ep => new { ep.EventId, ep.ParticipantId });
+
+            modelBuilder.Entity<EventParticipant>()
+                .HasOne(ep => ep.Event)
+                .WithMany(e => e.EventParticipants)
+                .HasForeignKey(ep => ep.EventId);
+
+            modelBuilder.Entity<EventParticipant>()
+                .HasOne(ep => ep.Participant)
+                .WithMany(p => p.EventParticipants)
+                .HasForeignKey(ep => ep.ParticipantId);
+
+            modelBuilder.Entity<SessionSpeaker>()
+                .HasKey(ss => new { ss.SessionId, ss.SpeakerId });
+
+            modelBuilder.Entity<SessionSpeaker>()
+                .HasOne(ss => ss.Session)
+                .WithMany(s => s.SessionSpeakers)
+                .HasForeignKey(ss => ss.SessionId);
+
+            modelBuilder.Entity<SessionSpeaker>()
+                .HasOne(ss => ss.Speaker)
+                .WithMany(s => s.SessionSpeakers)
+                .HasForeignKey(ss => ss.SpeakerId);
+
+            // Event
+            modelBuilder.Entity<Event>()
+                .Property(e => e.Title)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Event>()
+                .HasOne(e => e.Location)
+                .WithMany(l => l.Events)
+                .HasForeignKey(e => e.LocationId);
+
+            // Participant
+            modelBuilder.Entity<Participant>()
+                .Property(p => p.Email)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Participant>()
+                .HasIndex(p => p.Email)
+                .IsUnique();
+
+            // Session
+            modelBuilder.Entity<Session>()
+                .Property(s => s.Title)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Session>()
+                .HasOne(s => s.Event)
+                .WithMany(e => e.Sessions)
+                .HasForeignKey(s => s.EventId);
+
+            modelBuilder.Entity<Session>()
+                .HasOne(s => s.Room)
+                .WithMany(r => r.Sessions)
+                .HasForeignKey(s => s.RoomId);
+
+            // Room
+            modelBuilder.Entity<Room>()
+                .HasOne(r => r.Location)
+                .WithMany(l => l.Rooms)
+                .HasForeignKey(r => r.LocationId);
+
+            // Rating
+            modelBuilder.Entity<Rating>()
+                .HasOne(r => r.Session)
+                .WithMany(s => s.Ratings)
+                .HasForeignKey(r => r.SessionId);
+
+            modelBuilder.Entity<Rating>()
+                .HasOne(r => r.Participant)
+                .WithMany(p => p.Ratings)
+                .HasForeignKey(r => r.ParticipantId);
+
+            // Unicité : Un participant ne peut noter qu'une fois une session
+            modelBuilder.Entity<Rating>()
+                .HasIndex(r => new { r.SessionId, r.ParticipantId })
+                .IsUnique();
+        }
+    }
+
+}
