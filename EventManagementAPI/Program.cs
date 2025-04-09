c08a3c0e04821aee77fcf74296cb757e8059f3a7
kev <begot.kevin@gmail.com>
2025-04-09 12:33:10 +0200

    [ADD] clean file

--- EventManagementAPI/Program.cs
+++ EventManagementAPI/Program.cs
@@ -1,94 +1,104 @@
 using EventManagementAPI.Data;
+using EventManagementAPI.Mapping;
 using EventManagementAPI.Middlewares;
 using EventManagementAPI.Repositories;
 using EventManagementAPI.Repositories.Interfaces;
 using EventManagementAPI.Services;
 using EventManagementAPI.Services.Interfaces;
-using Microsoft.EntityFrameworkCore;
+using EventManagementAPI.Validators;
 using FluentValidation;
 using FluentValidation.AspNetCore;
-using EventManagementAPI.Validators;
-using EventManagementAPI.Mapping;
-
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// ===============================================
+// Configuration de la base de données
+// ===============================================
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
 );
 
-// Event validators
+// ===============================================
+// Configuration de FluentValidation
+// ===============================================
 builder.Services.AddValidatorsFromAssemblyContaining<CreateEventDTOValidator>();
-builder.Services.AddFluentValidationAutoValidation();
-
-// Location validators
 builder.Services.AddValidatorsFromAssemblyContaining<CreateLocationDTOValidator>();
-builder.Services.AddFluentValidationAutoValidation();
-
-// Participant validators
 builder.Services.AddValidatorsFromAssemblyContaining<CreateParticipantDTOValidator>();
 builder.Services.AddFluentValidationAutoValidation();
 
-// Event repository et service
+// ===============================================
+// Enregistrement des repositories et services
+// ===============================================
+// Event
 builder.Services.AddScoped<IEventRepository, EventRepository>();
 builder.Services.AddScoped<IEventService, EventService>();
 
-// Participant repository et service
+// Participant
 builder.Services.AddScoped<IParticipantRepository, ParticipantRepository>();
 builder.Services.AddScoped<IParticipantService, ParticipantService>();
 
-// Session repository et service
+// Session
 builder.Services.AddScoped<ISessionRepository, SessionRepository>();
 builder.Services.AddScoped<ISessionService, SessionService>();
 
-// Speaker repository et service
+// Speaker
 builder.Services.AddScoped<ISpeakerRepository, SpeakerRepository>();
 builder.Services.AddScoped<ISpeakerService, SpeakerService>();
 
-// Location repository et service
+// Location
 builder.Services.AddScoped<ILocationRepository, LocationRepository>();
 builder.Services.AddScoped<ILocationService, LocationService>();
 
-// Room repository et service
+// Room
 builder.Services.AddScoped<IRoomRepository, RoomRepository>();
 builder.Services.AddScoped<IRoomService, RoomService>();
 
-// EventParticipant repository et service
+// EventParticipant (Inscriptions)
 builder.Services.AddScoped<IEventParticipantRepository, EventParticipantRepository>();
 builder.Services.AddScoped<IRegistrationService, RegistrationService>();
 
-// Rating repository et service
+// Rating
 builder.Services.AddScoped<IRatingRepository, RatingRepository>();
 builder.Services.AddScoped<IRatingService, RatingService>();
 
-// SessionSpeaker repository et service
+// SessionSpeaker
 builder.Services.AddScoped<ISessionSpeakerRepository, SessionSpeakerRepository>();
 builder.Services.AddScoped<ISessionSpeakerService, SessionSpeakerService>();
 
+// ===============================================
 // Enregistrement d'AutoMapper
+// ===============================================
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
+// ===============================================
+// Ajout des services pour l'API et Swagger
+// ===============================================
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// ===============================================
+// Exécution du Seeding de la base de données
+// ===============================================
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     DatabaseSeeder.Seed(context);
 }
 
+// ===============================================
+// Configuration des Middlewares
+// ===============================================
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// Execption Middleware (error management)
 app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseHttpsRedirection();
